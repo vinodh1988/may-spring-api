3e31379a10a67365edbb216e67b881265a934696
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-06 15:35:30 +0530

    first-commit

---
+++ src/main/java/com/solution/FullRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullRestApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/solution/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.solution.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+	@RequestMapping("/hello")
+	public String home() {
+		return "home";
+	}
+
+}
