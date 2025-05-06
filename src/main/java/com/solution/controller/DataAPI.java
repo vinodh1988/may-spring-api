a7f1acfd9977976963a157e123b269531a667c56
vinodh1988 <k.c.vinodh@hotmail.com>
2025-05-06 17:38:59 +0530

    post

--- src/main/java/com/solution/controller/DataAPI.java
+++ src/main/java/com/solution/controller/DataAPI.java
@@ -3,6 +3,11 @@ package com.solution.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +21,13 @@ public class DataAPI {
 	@Autowired 
 	private DataService dataService;
 	@GetMapping("")
-    public List<Person> getPeople() {
-		return dataService.getPeople();
+    public ResponseEntity<List<Person>> getPeople() {
+		return ResponseEntity.ok(dataService.getPeople());
+	}
+	@PostMapping("")
+	public ResponseEntity<Person> addPerson(@RequestBody Person person) {
+		dataService.addPerson(person); 
+		return new ResponseEntity(person, HttpStatus.CREATED);
 	}
 	
 }
